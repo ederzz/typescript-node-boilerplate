8ed8232afd7bc3290a30224e0f2a911121f5a15e
shenyiling <jzx19951028yyjy@163.com>
2019-01-14 17:23:53 +0800

    feat: add /crawler/bookClassifyList --get route

--- dist/router/crawler.js
+++ dist/router/crawler.js
@@ -2,13 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Router = require("koa-router");
 const axios_1 = require("axios");
+const cheerio_1 = require("cheerio");
 const router = new Router({
     prefix: '/crawler'
 });
 router
-    .get('/test', async (ctx) => {
-    const result = await axios_1.default.get('https://www.baidu.com');
-    console.log(result);
-    ctx.body = 'test';
+    .get('/bookClassifyList', async (ctx) => {
+    const { status, data } = await axios_1.default.get('https://www.qidian.com/');
+    if (status !== 200) {
+        ctx.body = '请求失败';
+        return;
+    }
+    const $ = cheerio_1.load(data);
+    const classifyList = $('.classify-list dl').children();
+    const classifyListData = [];
+    classifyList.each((_, ele) => {
+        const classify = $('a > cite > .info > i', ele).text();
+        const bookNum = Number($('a > cite > .info b', ele).text());
+        classifyListData.push({
+            classify,
+            bookNum
+        });
+    });
+    ctx.body = classifyListData;
 });
 exports.default = router;
--- router/crawler.ts
+++ router/crawler.ts
@@ -1,15 +1,44 @@
 import * as Router from 'koa-router'
 import axios from 'axios'
+import {
+    load
+} from 'cheerio'
 
 const router = new Router({
     prefix: '/crawler'
 })
 
+interface ClassifyItem {
+    classify: string,
+    bookNum: number
+}
+
 router
-.get('/test', async (ctx) => {
-    const result = await axios.get('https://www.baidu.com')
-    console.log(result)
-    ctx.body = 'test'
+.get('/bookClassifyList', async (ctx) => {
+    const {
+        status,
+        data
+    } = await axios.get('https://www.qidian.com/')
+    if (status !== 200) {
+        ctx.body = '请求失败'
+        return 
+    }
+
+    const $: CheerioStatic = load(data)
+    const classifyList: Cheerio = $('.classify-list dl').children()
+    const classifyListData: ClassifyItem[] = []
+
+    classifyList.each((_, ele) => {
+        const classify: string = $('a > cite > .info > i', ele).text()
+        const bookNum: number = Number($('a > cite > .info b', ele).text())
+
+        classifyListData.push({
+            classify,
+            bookNum
+        })
+    })
+
+    ctx.body = classifyListData
 })
 
 export default router
