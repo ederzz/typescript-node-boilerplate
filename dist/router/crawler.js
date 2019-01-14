74d950a6f41de8a0fc492ad3d800d3c0779da16b
shenyiling <jzx19951028yyjy@163.com>
2019-01-14 17:56:37 +0800

    fix: add /crawler/rank-list --get route

--- dist/router/crawler.js
+++ dist/router/crawler.js
@@ -6,9 +6,10 @@ const cheerio_1 = require("cheerio");
 const router = new Router({
     prefix: '/crawler'
 });
+const siteAddress = 'https://www.qidian.com/';
 router
     .get('/bookClassifyList', async (ctx) => {
-    const { status, data } = await axios_1.default.get('https://www.qidian.com/');
+    const { status, data } = await axios_1.default.get(siteAddress);
     if (status !== 200) {
         ctx.body = '请求失败';
         return;
@@ -25,5 +26,41 @@ router
         });
     });
     ctx.body = classifyListData;
+})
+    .get('/rank-lists', async (ctx) => {
+    const { status, data } = await axios_1.default.get(siteAddress);
+    if (status !== 200) {
+        ctx.body = '请求失败';
+        return;
+    }
+    const $ = cheerio_1.load(data);
+    const rankLists = $('.rank-list-row > .rank-list');
+    const rankListData = [];
+    rankLists.each((_, ele) => {
+        const title = $('.wrap-title', ele).text();
+        const books = [];
+        $('.book-list > ul', ele).children().each((i, bookEle) => {
+            if (i === 0) {
+                const name = $('.book-wrap > .book-info > h4 > a', bookEle).text();
+                const total = Number($('.book-wrap > .book-info > .digital > em', bookEle).text());
+                books.push({
+                    name,
+                    total
+                });
+                return;
+            }
+            const name = $('.name-box .name', bookEle).text();
+            const total = $('.name-box .total', bookEle).text();
+            books.push({
+                name,
+                total: total && Number(total) || null
+            });
+        });
+        rankListData.push({
+            title,
+            books
+        });
+    });
+    ctx.body = rankListData;
 });
 exports.default = router;
--- router/crawler.ts
+++ router/crawler.ts
@@ -7,10 +7,19 @@ import {
 const router = new Router({
     prefix: '/crawler'
 })
+const siteAddress: string = 'https://www.qidian.com/' // 站点地址
 
 interface ClassifyItem {
     classify: string,
     bookNum: number
+}
+interface Book {
+    name: string,
+    total: number // 月票
+}
+interface RankList {
+    title: string,
+    books: Book[]
 }
 
 router
@@ -18,7 +27,7 @@ router
     const {
         status,
         data
-    } = await axios.get('https://www.qidian.com/')
+    } = await axios.get(siteAddress)
     if (status !== 200) {
         ctx.body = '请求失败'
         return 
@@ -39,6 +48,51 @@ router
     })
 
     ctx.body = classifyListData
+})
+.get('/rank-lists', async (ctx) => {
+    const {
+        status,
+        data
+    } = await axios.get(siteAddress)
+    if (status !== 200) {
+        ctx.body = '请求失败'
+        return
+    }
+
+    const $: CheerioStatic = load(data)
+    const rankLists: Cheerio = $('.rank-list-row > .rank-list')
+    const rankListData: RankList[] = [] // return data
+
+    rankLists.each((_, ele) => {
+        const title = $('.wrap-title', ele).text()
+        const books: Book[] = []
+
+        $('.book-list > ul', ele).children().each((i, bookEle) => {
+            if (i === 0) {
+                const name = $('.book-wrap > .book-info > h4 > a', bookEle).text()
+                const total = Number($('.book-wrap > .book-info > .digital > em', bookEle).text())
+                books.push({
+                    name,
+                    total
+                })
+                return
+            }
+            const name = $('.name-box .name', bookEle).text()
+            const total = $('.name-box .total', bookEle).text()
+
+            books.push({
+                name,
+                total: total && Number(total) || null
+            })
+        })
+
+        rankListData.push({
+            title,
+            books
+        })
+    })
+
+    ctx.body = rankListData
 })
 
 export default router
